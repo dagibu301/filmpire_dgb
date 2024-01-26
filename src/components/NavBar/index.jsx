c23bd3b22034a4c0619d5ac6059fadcdcd88ceff
David Giraldo <david@mvnifest.com>
2024-01-26 10:12:49 -0500

    Toolbar first version

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,34 @@
+// Libs
 import React from 'react';
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+} from '@mui/material';
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+// Syles
+import useStyles from './styles';
 
 const NavBar = () => {
-  console.log('NavBar');
-  return <div>NavBar</div>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>Hello world</Toolbar>
+      </AppBar>
+      <div>hola</div>
+    </>
+  );
 };
 
 export default NavBar;
