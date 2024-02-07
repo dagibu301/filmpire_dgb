bef6aa46469cd81e58238b599931956dfad41bea
David Giraldo <david@mvnifest.com>
2024-02-07 17:56:07 -0500

    feat: add movie list

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -4,6 +4,26 @@ export default makeStyles((theme) => ({
   movie: {
     padding: "10px",
   },
+  links: {
+    alignItems: "center",
+    fontWeight: "bolder",
+    [theme.breakpoints.up("xs")]: {
+      display: "flex",
+      flexDirection: "column",
+      textDecoration: "none",
+    },
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+  image: {
+    borderRadius: "20px",
+    height: "300px",
+    marginBottom: "10px",
+    "&:hover": {
+      transform: "scale(1.05)",
+    },
+  },
   title: {
     color: theme.palette.text.primary,
     textOverflow: "ellipsis",
@@ -12,6 +32,9 @@ export default makeStyles((theme) => ({
     overflow: "hidden",
     marginTop: "10px",
     marginBottom: 0,
+    textAlign: "center",
+  },
+  grow: {
     textAlign: "center",
   },
 }));
