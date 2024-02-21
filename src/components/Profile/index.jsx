c4612d300f0090642d9aebd37aa53efdee7a95b0
David Giraldo <david@mvnifest.com>
2024-02-20 21:18:39 -0500

    feat: add my profile information

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+// Libs
+import { useEffect } from "react";
+import { Box, Typography, Button } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
+// Store
+import { useSelector } from "react-redux";
+import { userSelector } from "../../features/auth";
 
 const Profile = () => {
-  console.log('Profile');
-  return <div>Profile</div>;
+  const { user } = useSelector(userSelector);
+  const favoriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
+
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile ...{user?.username}...
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant="h5">
+          Add favorites or watchlist to see them here!
+        </Typography>
+      ) : (
+        <Box>
+          Favorite movies
+        </Box>
+      )}
+    </Box>
+  );
 };
 
 export default Profile;
