be1fc5479de54d2624899ac3d6ef7f76c82001fc
David Giraldo <david@mvnifest.com>
2024-02-29 19:48:47 -0500

    feat: add avatar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -98,7 +98,11 @@ const NavBar = () => {
                     <Avatar
                       style={{ width: 30, height: 30 }}
                       alt="Profile"
-                      src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+                      src={
+                        user?.avatar?.tmdb?.avatar_path
+                          ? `https://image.tmdb.org/t/p/original/${user.avatar.tmdb.avatar_path}`
+                          : "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+                      }
                     />
                   </>
                 )}
